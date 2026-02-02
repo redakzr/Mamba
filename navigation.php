152c50e647108684990f94ccf86e4982bd9c9fb1
redakzr <reda.khezzar@etu.cyu.fr>
2026-02-02 17:46:08 +0100

    Add files via upload

---
+++ navigation.php
@@ -0,0 +1,10 @@
+<?php
+$barreNav = array(
+  "Home"=>"#hero",
+  "About"=>"#about",
+  "Services"=>"#services",
+  "Portfolio"=>"#portfolio",
+  "Team"=>"#team",
+  "Contact"=>"#contact"
+);
+?>
---
+++ social-media.php
@@ -0,0 +1,17 @@
+<?php
+/*
+$socialMedia = array(
+    "https://www.twitter.com/",
+    "https://www.facebook.com/",
+    "https://www.instagram.com/",
+    "https://www.linkedin.com/"
+);
+*/
+$socialMedia = array(
+    "twitter",
+    "facebook",
+    "instagram",
+    "linkedin"
+
+);
+?>
